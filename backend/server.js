c79de31c62db97ac25f7c050cdddcb700c06d495
Harman Singh <enduring.ramgarhia@gmail.com>
2026-07-06 14:58:01 +0530

    vedio limit update

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,8 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // ================= ROUTES =================
 app.use("/api/distance", distanceRoute);
