38b97f64bcbca9aec030bee38505d41f86811aff
Harman Singh <enduring.ramgarhia@gmail.com>
2026-08-26 17:23:31 +0530

    error update

--- backend/src/routes/bookingRoutes.js
+++ backend/src/routes/bookingRoutes.js
@@ -2,11 +2,8 @@ import express from "express";
 import {
   createBooking,
   getBookings,
-   getBookingById,
-  createOrder,
-  verifyPayment,
+  getBookingById,
   calculateDistance
-  // downloadReceipt
 } from "../controllers/bookingController.js";
 
 const router = express.Router();
@@ -14,10 +11,7 @@ const router = express.Router();
 router.post("/", createBooking);
 router.get("/bookings", getBookings);
 
-router.post("/create-order", createOrder);
-router.post("/verify-payment", verifyPayment);
 router.post("/calculate-distance", calculateDistance);
 router.get("/bookings/:id", getBookingById);
-// router.get("/receipt/:id", downloadReceipt);
 
 export default router;
