27d8571251d2318a9dd474a83e39f7b7e1bffa1f
Harman Singh <enduring.ramgarhia@gmail.com>
2026-08-29 17:05:40 +0530

    navbar adjust in mobile screen

--- turban_culture/src/Components/Navbar.jsx
+++ turban_culture/src/Components/Navbar.jsx
@@ -17,7 +17,8 @@ const Navbar = () => {
     <div className='fixed top-0 left-0 w-full bg-[#fdfaf6]/95 backdrop-blur-md z-50 border-b border-[#c9913a]/15 shadow-sm '>
 
       {/* Main Navbar */}
-      <div className='h-26 md:h-20 flex justify-between items-center px-4 md:px-8 lg:px-12'>
+      {/* Main Navbar */}
+      <div className='h-16 md:h-20 flex justify-between items-center px-3 md:px-8 lg:px-12'>
 
         {/* Logo */}
         <div
@@ -27,28 +28,31 @@ const Navbar = () => {
           <img
             src={transparent1}
             alt="logo"
-            className='w-36 md:w-52 h-16 md:h-20 object-contain hover:scale-105 transition-transform duration-300' />
+            className='w-24 md:w-52 h-12 md:h-20 object-contain hover:scale-105 transition-transform duration-300'
+          />
         </div>
 
         {/* Desktop Menu */}
         <div className='hidden md:flex gap-8 items-center text-sm font-medium'>
           <NavLink to="/">Home</NavLink>
-
           <NavLink to="/Services">Services</NavLink>
           <NavLink to="/Gallery">Gallery</NavLink>
-          {/* <NavLink to="/Reviews">Reviews</NavLink> */}
           <Button path="/Booking" Name="Book Now" />
         </div>
 
         {/* Mobile Hamburger */}
         <button
-          className='md:hidden w-9 h-9 flex flex-col justify-center items-center gap-1.5 text-[#c9913a]'
+          className='md:hidden w-8 h-8 flex flex-col justify-center items-center gap-1.5 text-[#c9913a]'
           onClick={() => setMenuOpen(!menuOpen)}
         >
-          <span className={`block w-6 h-0.5 bg-[#c9913a] transition-all duration-300 ${menuOpen ? 'rotate-45 translate-y-2' : ''}`} />
-          <span className={`block w-6 h-0.5 bg-[#c9913a] transition-all duration-300 ${menuOpen ? 'opacity-0' : ''}`} />
-          <span className={`block w-6 h-0.5 bg-[#c9913a] transition-all duration-300 ${menuOpen ? '-rotate-45 -translate-y-2' : ''}`} />
+          <span className={`block w-5 h-0.5 bg-[#c9913a] transition-all duration-300 ${menuOpen ? 'rotate-45 translate-y-2' : ''
+            }`} />
+          <span className={`block w-5 h-0.5 bg-[#c9913a] transition-all duration-300 ${menuOpen ? 'opacity-0' : ''
+            }`} />
+          <span className={`block w-5 h-0.5 bg-[#c9913a] transition-all duration-300 ${menuOpen ? '-rotate-45 -translate-y-2' : ''
+            }`} />
         </button>
+
       </div>
 
       {/* Mobile Menu */}
